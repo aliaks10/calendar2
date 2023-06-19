c718588c911e1abbdbcc6482a54ad4d8d1eed60c
Aliaksei Barysau <abarysau.dev@gmail.com>
2023-06-19 10:36:30 +0200

    first commit

---
+++ AppRouter.tsx
@@ -0,0 +1,28 @@
+import React, {FC, useState, useEffect} from 'react';
+import {Route, Routes} from "react-router-dom";
+import {Navigate} from "react-router-dom";
+
+const AppRouter: FC = () => {
+    const [isAuth, setIsAuth] = useState(false);
+    const [username, setUsername] = useState(false);
+
+    useEffect(() => {
+        if(localStorage.getItem("isAuth")) {
+            setIsAuth(true);
+            setUsername(localStorage.getItem("username") || "");
+        }
+    }, []);
+
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Navigate to={`/login`} replace/>}/>
+                <Route path={`/login`} element={<h1>Login</h1>}/>
+                <Route path={`/calendar`} element={<h1>Calendar</h1>}/>
+                <Route path="*" element={<Navigate to="/404" replace/>}/>
+            </Routes>
+        </>
+    );
+};
+
+export default AppRouter;
